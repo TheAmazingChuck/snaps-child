afe84a6bf320a28f84be396b92d9f7bf5f1705eb
Charles Levasseur <theamazingchuck@gmail.com>
2015-11-10 08:22:26 -0500

    ajout du home

---
+++ home-template.php
@@ -0,0 +1,4 @@
+<?php /* Template Name: home-template */ ?>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+
+<div id="home-page"><a href="<?php bloginfo('url'); ?>/index.php/blog/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/page-titre.png" /></a></div>
